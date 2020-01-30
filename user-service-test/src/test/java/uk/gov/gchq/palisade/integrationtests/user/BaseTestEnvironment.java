55896037148c0f772741447cfd32a3e116f31fb6
developer01189998819991197253 <ChrisCoDev@gmail.com>
2020-01-30 15:49:05 +0000

    pal-389-integration tests user service pom, dockerfile and values.yaml

---
+++ user-service-test/src/test/java/uk/gov/gchq/palisade/integrationtests/user/BaseTestEnvironment.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.palisade.integrationtests.user;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.ClassRule;
+
+import uk.gov.gchq.palisade.RequestId;
+import uk.gov.gchq.palisade.User;
+import uk.gov.gchq.palisade.UserId;
+import uk.gov.gchq.palisade.service.Service;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+public class BaseTestEnvironment {
+
+    @JsonPropertyOrder(value = {"class"}, alphabetic = true)
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "class")
+    @JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id")
+    public static class StubService implements Service {
+
+        @Override
+        public boolean equals(final Object obj) {
+            return this == obj || obj != null && getClass() == obj.getClass();
+        }
+    }
+
+    @ClassRule
+    public static WireMockRule USER_SERVICE_MOCK = new WireMockRule(options().port(8087).notifier(new ConsoleNotifier(true)));
+
+    RequestId REQUEST_ID = new RequestId().id("request-id");
+
+    UserId USER_ID = new UserId().id("user-id");
+    User USER = new User().userId(USER_ID);
+}
