03ad544e9c6120cb5efeba8043e65eadb596833a
ac74475 <ac74475@users.noreply.github.com>
2020-02-20 09:24:42 +0000

    Updated copyright years

--- palisade-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/palisade/mock/StubResource.java
+++ palisade-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/palisade/mock/StubResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Crown Copyright
+ * Copyright 2020 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,4 +47,3 @@ public class StubResource extends AbstractLeafResource {
         return comp.compare(this, (StubResource) o);
     }
 }
-
