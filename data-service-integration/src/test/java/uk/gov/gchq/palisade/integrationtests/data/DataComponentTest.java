450bddc09b9848bd21d520a9b8168c924960bab6
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-04-21 08:38:31 +0100

    Pal 573 removed unused imports

--- data-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/data/DataComponentTest.java
+++ data-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/data/DataComponentTest.java
@@ -64,7 +64,6 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 @EnableFeignClients
 @RunWith(SpringRunner.class)
