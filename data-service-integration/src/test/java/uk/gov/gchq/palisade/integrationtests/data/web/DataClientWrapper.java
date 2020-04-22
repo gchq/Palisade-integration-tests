9506d80b5b4df005c3a5b4aa86168fd8363b64f6
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-04-22 09:03:45 +0100

    Pal 573 added JavaDoc to DataClientWrapper class

--- data-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/data/web/DataClientWrapper.java
+++ data-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/data/web/DataClientWrapper.java
@@ -24,6 +24,8 @@ import org.slf4j.LoggerFactory;
 import uk.gov.gchq.palisade.data.serialise.AvroSerialiser;
 import uk.gov.gchq.palisade.data.serialise.Serialiser;
 import uk.gov.gchq.palisade.example.hrdatagenerator.types.Employee;
+import uk.gov.gchq.palisade.reader.common.DataFlavour;
+import uk.gov.gchq.palisade.resource.LeafResource;
 import uk.gov.gchq.palisade.service.Service;
 import uk.gov.gchq.palisade.service.data.request.AddSerialiserRequest;
 import uk.gov.gchq.palisade.service.data.request.ReadRequest;
@@ -45,11 +47,23 @@ public class DataClientWrapper implements Service {
         this.mapper = mapper;
     }
 
+    /**
+     * Passes the request to the data-service to be processed
+     *
+     * @param request   a {@link ReadRequest} containing the token and a {@link LeafResource} to be read
+     * @return          a {@link Stream} of {@link Employee} objects
+     */
     public Stream<Employee> readChunked(final ReadRequest request) {
         LOGGER.debug("Reading request: {}", request);
         return getFromFeignResponse(() -> client.readChunked(request));
     }
 
+    /**
+     * Converts the returned data into a stream of objects
+     *
+     * @param feignCall     a {@link Supplier} containing the {@link Response} from feign
+     * @return              a {@link Stream} of {@link Employee} objects
+     */
     private Stream<Employee> getFromFeignResponse(final Supplier<Response> feignCall) {
         try {
             InputStream responseStream = feignCall.get().body().asInputStream();
@@ -62,10 +76,21 @@ public class DataClientWrapper implements Service {
         }
     }
 
+    /**
+     * Add a serialiser to the data-service so that it can deserialise the data
+     *
+     * @param request   an {@link AddSerialiserRequest} that conatins the {@link Serialiser} and the {@link DataFlavour}
+     * @return          a {@link Boolean} value
+     */
     public Boolean addSerialiser(final AddSerialiserRequest request) {
         return client.addSerialiser(request);
     }
 
+    /**
+     * Get the health of the service
+     *
+     * @return      a feign {@link Response} containing the service health details
+     */
     public Response getHealth() {
         try {
             return this.client.getHealth();
