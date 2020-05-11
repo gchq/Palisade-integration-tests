041750e91a5cc3f2bf6aee7b5ed785372af33df2
developer01189998819991197253 <ChrisCoDev@gmail.com>
2020-05-11 11:34:33 +0100

    updated PolicyTestConfiguration

--- policy-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/policy/config/PolicyTestConfiguration.java
+++ policy-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/policy/config/PolicyTestConfiguration.java
@@ -20,8 +20,10 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import uk.gov.gchq.palisade.service.PolicyConfiguration;
+import uk.gov.gchq.palisade.service.ResourceConfiguration;
 import uk.gov.gchq.palisade.service.UserConfiguration;
 import uk.gov.gchq.palisade.service.policy.config.StdPolicyConfiguration;
+import uk.gov.gchq.palisade.service.policy.config.StdResourceConfiguration;
 import uk.gov.gchq.palisade.service.policy.config.StdUserConfiguration;
 import uk.gov.gchq.palisade.service.policy.service.NullPolicyService;
 import uk.gov.gchq.palisade.service.policy.service.PolicyService;
@@ -33,20 +35,20 @@ import uk.gov.gchq.palisade.service.policy.service.PolicyService;
 
 @TestConfiguration
 public class PolicyTestConfiguration {
-
     @Bean
     public PolicyConfiguration policyConfiguration() {
         return new StdPolicyConfiguration();
     }
-
     @Bean
     public UserConfiguration userConfiguration() {
         return new StdUserConfiguration();
     }
-
+    @Bean
+    public ResourceConfiguration resourceConfiguration() {
+        return new StdResourceConfiguration();
+    }
     @Bean
     public PolicyService policyService() {
         return new NullPolicyService();
     }
-
 }
