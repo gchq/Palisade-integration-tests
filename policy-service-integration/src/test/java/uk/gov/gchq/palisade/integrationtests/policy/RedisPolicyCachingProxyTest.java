06bf674f6403ccf1e30877e2ea47fd01f8a9daab
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-08-25 15:27:12 +0100

    Added RedisTestConfig and RedisPolicyCachingProxyTest, renamed original policyCachingProxyTest to CaffeinePolicyCachingProxyTest, added redis to pom

--- policy-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/policy/RedisPolicyCachingProxyTest.java
+++ policy-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/policy/RedisPolicyCachingProxyTest.java
@@ -15,43 +15,35 @@
  */
 package uk.gov.gchq.palisade.integrationtests.policy;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 
 import uk.gov.gchq.palisade.integrationtests.policy.config.PolicyTestConfiguration;
-import uk.gov.gchq.palisade.policy.PassThroughRule;
+import uk.gov.gchq.palisade.integrationtests.policy.config.RedisTestConfiguration;
 import uk.gov.gchq.palisade.resource.Resource;
-import uk.gov.gchq.palisade.resource.StubResource;
 import uk.gov.gchq.palisade.resource.impl.FileResource;
 import uk.gov.gchq.palisade.resource.impl.SystemResource;
-import uk.gov.gchq.palisade.service.SimpleConnectionDetail;
 import uk.gov.gchq.palisade.service.policy.PolicyApplication;
 import uk.gov.gchq.palisade.service.policy.service.PolicyService;
 import uk.gov.gchq.palisade.service.policy.service.PolicyServiceCachingProxy;
 import uk.gov.gchq.palisade.service.request.Policy;
 
 import java.util.Optional;
-import java.util.function.Function;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assume.assumeTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @Import(PolicyTestConfiguration.class)
-@SpringBootTest(classes = { PolicyApplication.class}, webEnvironment = WebEnvironment.NONE)
+@ActiveProfiles("redis")
+@SpringBootTest(classes = {PolicyApplication.class, RedisTestConfiguration.class}, webEnvironment = WebEnvironment.NONE)
 @ComponentScan(basePackages = "uk.gov.gchq.palisade")
-public class PolicyCachingProxyTest extends PolicyTestCommon {
+public class RedisPolicyCachingProxyTest extends PolicyTestCommon {
 
     @Autowired
     private PolicyServiceCachingProxy cacheProxy;
@@ -60,25 +52,25 @@ public class PolicyCachingProxyTest extends PolicyTestCommon {
     @Qualifier("impl")
     private PolicyService policyService;
 
-    @Before
+    @BeforeEach
     public void setup() {
         // Add the system resource to the policy service
-        assertThat(cacheProxy.setResourcePolicy(TXT_SYSTEM, TXT_POLICY), CoreMatchers.equalTo(TXT_POLICY));
+        assertThat(cacheProxy.setResourcePolicy(TXT_SYSTEM, TXT_POLICY)).isEqualTo(TXT_POLICY);
 
         // Add the directory resources to the policy service
-        assertThat(cacheProxy.setResourcePolicy(JSON_DIRECTORY, JSON_POLICY), CoreMatchers.equalTo(JSON_POLICY));
-        assertThat(cacheProxy.setResourcePolicy(SECRET_DIRECTORY, SECRET_POLICY), CoreMatchers.equalTo(SECRET_POLICY));
+        assertThat(cacheProxy.setResourcePolicy(JSON_DIRECTORY, JSON_POLICY)).isEqualTo(JSON_POLICY);
+        assertThat(cacheProxy.setResourcePolicy(SECRET_DIRECTORY, SECRET_POLICY)).isEqualTo(SECRET_POLICY);
 
         // Add the file resources to the policy service
         for (FileResource fileResource : FILE_RESOURCES) {
-            assertThat(cacheProxy.setResourcePolicy(fileResource, PASS_THROUGH_POLICY), CoreMatchers.equalTo(PASS_THROUGH_POLICY));
+            assertThat(cacheProxy.setResourcePolicy(fileResource, PASS_THROUGH_POLICY)).isEqualTo(PASS_THROUGH_POLICY);
         }
     }
 
     @Test
     public void contextLoads() {
-        assertNotNull(policyService);
-        assertNotNull(cacheProxy);
+        assertThat(policyService).isNotNull();
+        assertThat(cacheProxy).isNotNull();
     }
 
     @Test
@@ -91,7 +83,7 @@ public class PolicyCachingProxyTest extends PolicyTestCommon {
             Optional<Policy> policy = cacheProxy.getPolicy(resource);
 
             // Then
-            assertTrue(policy.isPresent());
+            assertThat(policy).isNotNull();
         }
     }
 
@@ -103,34 +95,13 @@ public class PolicyCachingProxyTest extends PolicyTestCommon {
         Optional<Policy> policy = cacheProxy.getPolicy(new FileResource().id("does not exist").type("null").serialisedFormat("null").parent(new SystemResource().id("also does not exist")));
 
         // Then
-        assertTrue(policy.isEmpty());
-    }
-
-    @Test
-    public void cacheMaxSizeTest() {
-        /// Given - the cache is overfilled
-        Function<Integer, Resource> makeResource = i -> new StubResource(i.toString(), i.toString(), i.toString(), new SimpleConnectionDetail().serviceName(i.toString()));
-        Function<Integer, Policy> makePolicy = i -> new Policy<>().resourceLevelRule(i.toString(), new PassThroughRule<>());
-        for (int count = 0; count <= 100; ++count) {
-            cacheProxy.setResourcePolicy(makeResource.apply(count), makePolicy.apply(count));
-        }
-
-        // When - an old entry is requested
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Optional<Policy> cachedPolicy = cacheProxy.getPolicy(makeResource.apply(0));
-
-        // Then - it has been evicted
-        assertTrue(cachedPolicy.isEmpty());
+        assertThat(policy).isEmpty();
     }
 
     @Test
     public void cacheTtlTest() {
         // Given - the requested resource has policies available
-        assumeTrue(cacheProxy.getPolicy(ACCESSIBLE_JSON_TXT_FILE).isPresent());
+        assertThat(cacheProxy.getPolicy(ACCESSIBLE_JSON_TXT_FILE)).isNotNull();
         // Given - a sufficient amount of time has passed
         try {
             Thread.sleep(2500);
@@ -142,6 +113,6 @@ public class PolicyCachingProxyTest extends PolicyTestCommon {
         Optional<Policy> cachedPolicy = cacheProxy.getPolicy(ACCESSIBLE_JSON_TXT_FILE);
 
         // Then - it has been evicted
-        assertTrue(cachedPolicy.isEmpty());
+        assertThat(cachedPolicy).isEmpty();
     }
 }
