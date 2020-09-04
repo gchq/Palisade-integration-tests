d7a6d04316ac0c3bd17d0d75c4e965ac12a6f717
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-09-04 15:28:30 +0100

    Fixed checkstyle in ScenarioPersistenceTest

--- resource-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/resource/ScenarioPersistenceTest.java
+++ resource-service-integration/src/test/java/uk/gov/gchq/palisade/integrationtests/resource/ScenarioPersistenceTest.java
@@ -28,7 +28,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import uk.gov.gchq.palisade.integrationtests.resource.config.ResourceTestConfiguration;
